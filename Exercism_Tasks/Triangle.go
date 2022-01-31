fd25c5eca5eb2ad0f26100c1ee14f3e0383ccc25
Sumathy-Sridhar <sumathysridhar22@gmail.com>
2022-01-31 15:33:17 +0530

    Exercism

---
+++ Exercism_Tasks/Triangle.go
@@ -0,0 +1,45 @@
+package main
+
+// Notice KindFromSides() returns this type. Pick a suitable data type.
+type Kind string
+
+const (
+	// Pick values for the following identifiers used by the test program.
+	NaT Kind = "Not a triangle"
+	Equ Kind = "Equilateral"
+	Iso Kind = "Isosceles"
+	Sca Kind = "Scalene"
+)
+
+// KindFromSides should have a comment documenting it.
+func KindFromSides(a, b, c float64) Kind {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	if a > b {
+		a, b = b, a
+	}
+	if b > c {
+		b, c = c, b
+	}
+	if a > b {
+		a, b = b, a
+	}
+	// sides are now sorted
+	switch {
+	case a <= 0:
+		return NaT
+	case a+b < c: // triangle inequality
+		return NaT
+	case a == c:
+		return Equ
+	case a == b || b == c:
+		return Iso
+	}
+	return Sca
+}
+
+func main() {
+	KindFromSides(10, 20, 30)
+}
