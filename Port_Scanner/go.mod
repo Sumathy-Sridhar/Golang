4f66d44e65d97f2c2761738541cd94d2d2fb6062
Sumathy-Sridhar <sumathysridhar22@gmail.com>
2022-02-21 13:23:58 +0530

    portscanner

---
+++ Port_Scanner/go.mod
@@ -0,0 +1,5 @@
+module Port_Scanner
+
+go 1.16
+
+require github.com/kelvin-mai/go-port-scanner v0.0.0-20201011175856-0c87e2290d2e
---
+++ Port_Scanner/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelvin-mai/go-port-scanner/port"
+)
+
+func main() {
+	start := time.Now()
+	port.GetOpenPorts("www.amazon.com", port.PortRange{Start: 75, End: 85})
+
+	// called with ip address
+	port.GetOpenPorts("104.26.10.78", port.PortRange{Start: 8079, End: 8090})
+
+	// verbose called with ip address and no host name returned -- single open port
+	port.GetOpenPorts("104.26.10.78", port.PortRange{Start: 440, End: 450})
+
+	// verbose called with ip adress and valid host name returned -- single open port
+	port.GetOpenPorts("137.74.187.104", port.PortRange{Start: 440, End: 450})
+
+	// verbose called with host name -- multiple ports returned
+	port.GetOpenPorts("scanme.nmap.org", port.PortRange{Start: 20, End: 80})
+	elapsed := time.Since(start)
+	fmt.Printf("Scan duration: %s", elapsed)
+}
